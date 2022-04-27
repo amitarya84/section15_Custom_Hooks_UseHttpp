8fd820ed0d1839a2b6e7ffae15de82dbdc519759
= <amitaryawrk@gmail.com>
2022-04-28 01:18:38 +0530

    http_requests_with_custom_hooks

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useFetch from './Hooks/useFetch';
+
+function App() {
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  
+  const { isLoading, error, sendRequest: fetchTasks } = useFetch();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://fake-data-api-default-rtdb.asia-southeast1.firebasedatabase.app/task.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://fake-data-api-default-rtdb.asia-southeast1.firebasedatabase.app/task.json'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import useFetch from '../../Hooks/useFetch';
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useFetch();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: 'https://fake-data-api-default-rtdb.asia-southeast1.firebasedatabase.app/task.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
